3c26c5146385bb368f35fce04c291290cd7a6d4e
Gaetan <gaetan@protonmail.com>
2019-10-21 10:30:46 +0200

    add the npm modules folder in the branch

--- breakit/src/App.js
+++ breakit/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './components/Footer'
 import './App.css';
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
     return (
       <div>
         <p>Ici tout commence</p>
+       
       </div>
     );
   }
---
+++ breakit/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css'
+
+
+class Footer extends React.Component {
+    render() {
+  
+  
+      return (
+        <div className="container">
+         
+         
+        </div>
+      );
+    }
+  }
+  
+  export default Footer;
+  
