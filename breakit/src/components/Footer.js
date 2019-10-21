3729dd355d8f9d3ceff45234c342e00b8ee6cfbc
Gaetan <gaetan@protonmail.com>
2019-10-21 12:09:02 +0200

    add the footer

--- breakit/src/components/Footer.js
+++ breakit/src/components/Footer.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './Footer.css'
-
-
+import map from '../components/img/mapportrait.png'
+import pictoMail from '../components/img/Mail-picto.svg'
 class Footer extends React.Component {
     render() {
   
   
       return (
-        <div className="container">
-         
-         
+       
+        <div>
+          
+          <div className="map">
+              <img src={map}></img>
+          </div>
+          <footer className="container">
+            <div className="buttonDiv">
+
+            <button className="button">
+              A proximité
+            </button>
+
+            <div className="pictoMail">
+            <img src={pictoMail}></img>
+            </div>
+            </div>
+          
+          </footer>
+
         </div>
       );
     }
