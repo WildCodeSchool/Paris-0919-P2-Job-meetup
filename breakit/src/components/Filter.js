adc1a5ae11e811f84481b34f3bdf97337c59226b
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2019-10-21 10:30:33 +0200

    modifs depart

---
+++ breakit/src/components/Filter.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './Filter.css'
+
+class filter extends React.Component {
+    render() {
+        return (
+
+            
+        )
+    }
+}
+
+export default Filter;
