821692dd5ec69032234929dd2943720d5c6ef5bb
Tim <Tim@MacBook-Pro-de-Tim.local>
2019-10-22 18:22:29 +0200

    create list

--- breakit/src/App.js
+++ breakit/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ListFilter from './components/ListFilter';
 import './App.css';
 
 class App extends React.Component {
@@ -7,7 +8,7 @@ class App extends React.Component {
 
     return (
       <div>
-        <p>Ici tout commence</p>
+        <ListFilter />
       </div>
     );
   }
