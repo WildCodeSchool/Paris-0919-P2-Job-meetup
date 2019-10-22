189588925d28d8340ac2ee11413049289975cbbd
Flauz <florianguillot94@gmail.com>
2019-10-22 18:00:17 +0200

    modifications css checkbox/button

--- breakit/src/components/Filter.js
+++ breakit/src/components/Filter.js
@@ -1,10 +1,12 @@
 import React from "react"
+import InputRange from "react-input-range";
+import 'react-input-range/lib/css/index.css'
 import './Filter.css'
-
 class Filter extends React.Component {
 
     state = {
-        openFilter: false
+        openFilter: false,
+        value: 2
     }
 
     render() {
@@ -20,16 +22,36 @@ class Filter extends React.Component {
                     >Filter</button>
                 </div>
                 <form className="checkBox">
-                    <label>
-                        <input type="checkbox" value="Meet-up" ></input>
-                        Meet-up
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Entreprises" ></input>
-                        Entreprises
-                    </label>
+                    <div className="check-meet-up">
+                        <label>
+                            <input type="checkbox" value="Meet-up" ></input>
+                            Meet-up
+                        </label>
+                    </div>
+                    <div className="check-entreprise">
+                        <label>
+                            <input type="checkbox" value="Entreprises" ></input>
+                            Entreprises
+                        </label>
+                    </div>
+                    <div className="check-cowork">
+                        <label>
+                            <input type="checkbox" value="Cafés-Coworking" ></input>
+                            Cafés-Coworking
+                        </label>
+                    </div>
+                    <div className="range">
+                        <p>Rayon</p>
+                        <InputRange
+                            maxValue={15}
+                            minValue={0}
+                            value={this.state.value}
+                            onChange={value => this.setState({ value })} />
+                    </div>
                 </form>
-            </div>    
+
+
+            </div >
         )
     }
 }
