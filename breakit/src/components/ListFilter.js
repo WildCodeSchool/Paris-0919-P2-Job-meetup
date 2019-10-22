6e845fa168d86e5ecab0c71d681899851460e2ff
Tim <Tim@MacBook-Pro-de-Tim.local>
2019-10-22 18:26:31 +0200

    add list

---
+++ breakit/src/components/ListFilter.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './ListFilter.css';
+
+
+class listFilter extends React.Component {
+    render() {
+        return (
+            <div className="first_container">
+                <div className="second_container">
+                    <div className="list">
+                        <ul>
+                            <li>Meetup React.js (350m)</li>
+                            <li>Blabla JS (500m)</li>
+                            <li>Meetup de folie (649m)</li>
+                            <li>Sandwich corporation (864m)</li>
+                            <li>Choucroute.JS (1432 m)</li>
+                            <li>Meetup React.js (350m)</li>
+                            <li>Blabla JS (500m)</li>
+                            <li>Meetup de folie (649m)</li>
+                            <li>Sandwich corporation (864m)</li>
+                            <li>Choucroute.JS (1432 m)</li>
+                            <li>Meetup React.js (350m)</li>
+                            <li>Blabla JS (500m)</li>
+                            <li>Meetup de folie (649m)</li>
+                            <li>Sandwich corporation (864m)</li>
+                            <li>Choucroute.JS (1432 m)</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default listFilter
