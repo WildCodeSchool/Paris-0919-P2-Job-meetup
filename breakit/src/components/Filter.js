0117430384b3721a3bfde8b6063755e8d08df9c0
Flauz <florianguillot94@gmail.com>
2019-10-22 14:56:48 +0200

    Modifications : add of the inputs checkbox

--- breakit/src/components/Filter.js
+++ breakit/src/components/Filter.js
@@ -2,30 +2,36 @@ import React from "react"
 import './Filter.css'
 
 class Filter extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            openFilter: false
-        }
+
+    state = {
+        openFilter: false
     }
+
     render() {
         return (
-            <div className="buttonfilter">
-                <button
-                    className={this.state.openFilter ? 'is-open' : ' '}
-                    onClick={event => {
-                        const newOpenFilter = !this.state.openFilter;
-                        this.setState({ openFilter: newOpenFilter });
-                    }}
-                >Filter</button>
-            </div>
-            
-             <div className={this.state.openFilter ? ' ' : 'is-open'}>
-                <input>Meetup</input>
-            </div>
-
-                )
+            <div className="filter">
+                <div className="buttonfilter">
+                    <button
+                        className={this.state.openFilter ? 'is-open' : ' '}
+                        onClick={event => {
+                            const newOpenFilter = !this.state.openFilter;
+                            this.setState({ openFilter: newOpenFilter });
+                        }}
+                    >Filter</button>
+                </div>
+                <div className="checkBox">
+                    <label>
+                        <input type="checkbox" value="Meet-up" checked></input>
+                        Meet-up
+                    </label>
+                    <label>
+                        <input type="checkbox" value="Entreprises" checked></input>
+                        Entreprises
+                    </label>
+                </div>
+            </div>    
+        )
     }
 }
 
-export default Filter;
+export default Filter
