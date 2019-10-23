7b60826580e8bd1043f763e94bd9922558b1f239
Tim <Tim@MacBook-Pro-de-Tim.local>
2019-10-23 18:39:08 +0200

    add welcome css and js

---
+++ breakit/src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Welcome.css';
+
+class Welcome extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                
+
+
+            </div>
+        )
+
+
+    }
+}
