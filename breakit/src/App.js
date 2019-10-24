668a1e50b0a3721c26d6c8804de3e046dc09b872
Tim <Tim@MacBook-Pro-de-Tim.local>
2019-10-24 09:56:02 +0200

    add app welcome logo svg and package

--- breakit/src/App.js
+++ breakit/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import Footer from './components/Footer';
-import ListFilter from './components/ListFilter';
 import './App.css';
-import Filter from './components/Filter';
-import MenuBurger from './components/MenuBurger';
-import SimpleExample from './components/SimpleExample'
+import Welcome from './components/Welcome'
 
 class App extends React.Component {
   state = {
@@ -18,11 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <MenuBurger className="menuburger" />
-        <SimpleExample className="simpleexample"/>
-        {this.state.isDisplayed? null : <Filter className="filter"/>}
-        {this.state.isDisplayed? <ListFilter className="listfilter"/> : null}
-        <Footer className="footer" footerClick={this.footerClick}/>
+        <Welcome />
       </div>
     );
   }
--- breakit/src/components/Welcome.js
+++ breakit/src/components/Welcome.js
@@ -4,13 +4,18 @@ import './Welcome.css';
 class Welcome extends React.Component {
     render() {
         return (
-            <div className="container">
-                
+            <div className="screen">
+                <div className="logo"></div>
+                <div className="container">
+                    <p className="bienvenue">Bienvenue !</p>
 
 
+                </div>
             </div>
         )
 
 
     }
 }
+
+export default Welcome ;
