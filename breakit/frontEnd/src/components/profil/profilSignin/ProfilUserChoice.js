9b3b095be0da867ee33510e35ae40713305b7beb
Gaetan <gaetan@protonmail.com>
2019-11-04 15:51:03 +0100

    add a structure of the skeletton for the choice user

---
+++ breakit/frontEnd/src/components/profil/profilSignin/ProfilUserChoice.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import logo from '../components/img/logo-blancjaune.svg'
+import './UserChoice.css'
+
+
+class UserChoice extends React.Component {
+    render() {
+        return(
+            <div className="containerUserProfil">
+                <img src={logo} alt="logo Skills" className="logoUserProfil"></img>
+                <div>
+                    <p className="inscriptionUserChoice">Profil</p>
+                    <p className="inscriptionUserChoice">Vous êtes…</p>
+                </div>
+
+                <div className="containerChoice">
+                    <form className="form">
+                        <p> <button type="button" id="userDev-button">Un développeur</button> </p>
+                        <p> <button type="button" id="userSociety-button">Un recruteur</button> </p>
+                    </form>
+                </div>
+
+                <div className="linearBalls" >
+                    <span className="doty" onclick="currentSlide(1)"></span>
+                    <span className="dot" onclick="currentSlide(2)"></span>
+                    <span className="dot" onclick="currentSlide(3)"></span>
+                    <span className="dot" onclick="currentSlide(4)"></span>
+                    <span className="dot" onclick="currentSlide(5)"></span>
+                    <span className="dot" onclick="currentSlide(6)"></span>                    
+                </div>
+                
+
+
+            </div>
+        )
+    }
+}
+
+export default UserChoice
