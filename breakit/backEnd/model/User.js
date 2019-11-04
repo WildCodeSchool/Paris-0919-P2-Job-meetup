67498a2264dda1a2c1c5661be9caaa4e14385fce
Alexis Blanchard <alexis.blanchard0@gmail.com>
2019-11-04 11:57:06 +0100

    user schema added as mongoose schema in User.js

---
+++ breakit/backEnd/model/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const userSchema =  mongoose.Schema(
+  {
+     firstName: {
+                type: String,
+                required: true,
+                maxlength: 50
+            },
+            lastName: {
+                type: String,
+                required: true,
+                maxlength: 50
+            },
+            mail: {
+                type: String,
+                required: true,
+                minlength: 5,
+                maxlength: 255,
+                unique: true
+            },
+            password: {
+                type: String,
+                required: true,
+                minlength: 5,
+                maxlength: 1024
+            },
+            type: {
+                type :String,
+                required : true,
+            },
+            skills: {
+                type : String,
+                required : true,
+                maxlength: 1024,
+            }
+        
+  },
+  { timestamps: true }
+);
