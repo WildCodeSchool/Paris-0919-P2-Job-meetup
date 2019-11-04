477089393d63fa7608c6dfb471fe262b11d772bf
Tim <Tim@MacBook-Pro-de-Tim.local>
2019-11-04 15:08:32 +0100

    create the skeleton of the first Userchchoice

--- breakit/src/components/UserChoice.js
+++ breakit/src/components/UserChoice.js
@@ -11,16 +11,24 @@ class UserChoice extends React.Component {
                 <div>
                     <p className="inscriptionUserChoice">Profil</p>
                     <p className="inscriptionUserChoice">Vous êtes…</p>
-
                 </div>
-
 
                 <div className="containerChoice">
                     <form className="form">
                         <p> <button type="button" id="userDev-button">Un développeur</button> </p>
-                        <p> <button type="button" id="userSociety-button">Une société</button> </p>
+                        <p> <button type="button" id="userSociety-button">Un recruteur</button> </p>
                     </form>
                 </div>
+
+                <div className="linearBalls" >
+                    <span className="doty" onclick="currentSlide(1)"></span>
+                    <span className="dot" onclick="currentSlide(2)"></span>
+                    <span className="dot" onclick="currentSlide(3)"></span>
+                    <span className="dot" onclick="currentSlide(4)"></span>
+                    <span className="dot" onclick="currentSlide(5)"></span>
+                    <span className="dot" onclick="currentSlide(6)"></span>                    
+                </div>
+                
 
 
             </div>
