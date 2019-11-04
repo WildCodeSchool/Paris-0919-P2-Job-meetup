e6515fcdcb3480503797400a00e49b54f3f4f66c
Alexis Blanchard <alexis.blanchard0@gmail.com>
2019-11-04 12:02:19 +0100

    model user exported

--- breakit/backEnd/model/User.js
+++ breakit/backEnd/model/User.js
@@ -2,40 +2,51 @@ const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
 
 const userSchema =  mongoose.Schema(
-  {
-     firstName: {
-                type: String,
-                required: true,
-                maxlength: 50
-            },
-            lastName: {
-                type: String,
-                required: true,
-                maxlength: 50
-            },
-            mail: {
-                type: String,
-                required: true,
-                minlength: 5,
-                maxlength: 255,
-                unique: true
-            },
-            password: {
-                type: String,
-                required: true,
-                minlength: 5,
-                maxlength: 1024
-            },
-            type: {
-                type :String,
-                required : true,
-            },
-            skills: {
-                type : String,
-                required : true,
-                maxlength: 1024,
-            }
+    {
+        firstName: 
+        {
+            type: String,
+            required: true,
+            maxlength: 50
+        },
+        lastName: 
+        {
+            type: String,
+            required: true,
+            maxlength: 50
+        },
+        mail: 
+        {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 255,
+            unique: true
+        },
+        password: 
+        {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 1024
+        },
+        type: 
+        {
+            type :String,
+            required : true,
+        },
+        skills: 
+        {
+            type : String,
+            required : true,
+            maxlength: 1024,
+        }
         
-  },
-  { timestamps: true }
+    },
+    { timestamps: true }
 );
+
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {User};
