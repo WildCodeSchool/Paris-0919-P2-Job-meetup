2edeab3c206c0e52bd25e110e4977af67461c781
Alexis Blanchard <alexis.blanchard0@gmail.com>
2019-11-04 12:09:29 +0100

    hash password function with bcrypt added to User.js

--- breakit/backEnd/model/User.js
+++ breakit/backEnd/model/User.js
@@ -46,6 +46,25 @@ const userSchema =  mongoose.Schema(
     { timestamps: true }
 );
 
+let SALT = 10;
+
+userSchema.pre('save', function(next){
+  const user = this;
+  if(user.isModified('password')) {
+    bcrypt.genSalt(SALT, function(err, salt){
+      if(err) return next(err);
+
+      bcrypt.hash(user.password, salt, function(err, hash){
+        if(err) return next(err);
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next()
+  }
+})
+
 
 const User = mongoose.model('User', userSchema)
 
