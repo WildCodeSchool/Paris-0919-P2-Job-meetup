f35125efced7ca3bf1a8c5ced494c7137e82376d
Gaetan <gaetan@protonmail.com>
2019-11-05 11:06:37 +0100

    install the begining of router

--- breakit/frontEnd/src/App.js
+++ breakit/frontEnd/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ProfilPicture from './components/profil/profilSignin/ProfilPicture'
+import ProfilDescription from './components/profil/profilSignin/ProfilDescription'
+import ProfilDevSpec from './components/profil/profilSignin/ProfilDevSpec'
+import ProfilInterests from './components/profil/profilSignin/ProfilInterests'
+import ProfilUserChoice from './components/profil/profilSignin/ProfilUserChoice'
 
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -11,9 +16,10 @@ class App extends React.Component {
     return (
       <div>
        
-       
-        <ProfilPicture/>
-
+       <Switch>
+         <Route exact path="/" component={}
+       </Switch>
+        
       </div>
     );
   }
