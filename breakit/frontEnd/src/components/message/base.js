624de61e1418d4461d61edc2e7dae7858c16d69d
Alexis Blanchard <alexis.blanchard0@gmail.com>
2019-11-15 09:33:13 +0100

    user ID added to chat

--- breakit/frontEnd/src/components/message/base.js
+++ breakit/frontEnd/src/components/message/base.js
@@ -6,6 +6,7 @@ const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyDgZXrdO3FX0rHHbv69FNBkVNkQhdxL5VE",
   authDomain: "chat-box-bdf73.firebaseapp.com",
   databaseURL: "https://chat-box-bdf73.firebaseio.com",
+  messagingSenderId: "GoNXiRi5MOMEtKmTecZ1U9o9Wcm2"
 })
 
 const base = Rebase.createClass(firebase.database())
